03332fd834fd7792cca93108bf69ba8ac6ed98fc
suchang <sotyo@icloud.com>
2018-08-03 16:29:50 +0900

    Test401 修改

--- src/main/java/test4/Test401.java
+++ src/main/java/test4/Test401.java
@@ -4,4 +4,12 @@ package test4;
  * Created by heqiming on 2018/08/03
  */
 public class Test401 {
+	public Test401() {
+		System.out.println("this is 401");
+	}
+
+	public static void main(String[] args) {
+		Test401 test = new Test401();
+
+	}
 }
